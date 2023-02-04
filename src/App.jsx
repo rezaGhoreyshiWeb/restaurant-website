a54971537205afd7b82cc55f6372d63264e9ca67
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-02-04 15:43:30 +0330

    add each component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./components";
 
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
